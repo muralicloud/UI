cf99fd40bd7a776b54509976262b567e1e8b98b3
Sunny <ambaraiya@gmail.com>
2020-11-30 20:55:18 -0800

    fnone added to file

---
+++ someclass.java
@@ -0,0 +1,11 @@
+public class someclass {
+	public void fnOne() {
+			System.out.println("I couldn't repair your brakes, so I made your horn louder]");
+		
+	}
+	public static void main(String[] args) {
+		someclass sc = new someclass();
+		sc.fnOne();
+        
+	}
+}
